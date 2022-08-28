61ab7e0a98b10aad4036cf6c3eb8215b8c92e23f
Jennifer Herr <jenh11@gmail.com>
2022-08-27 20:27:22 -0700

    Add sound functionality

--- index.js
+++ index.js
@@ -1,9 +1,97 @@
-var numberOfButtons = document.querySelectorAll(".drum").length;
+document.addEventListener("keydown", makeSound);
+document.addEventListener("click", handleClick);
 
 function handleClick() {
-  alert("I got clicked.");
+	var numberOfButtons = document.querySelectorAll(".drum").length;
+	for (var i = 0; i < numberOfButtons; ++i) {
+		document.querySelectorAll(".drum")[i].addEventListener("click", makeSound);
+	}
 }
 
-for (var i = 0; i < numberOfButtons; ++i) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+function makeSound(event) {
+	switch (event.key) {
+		case "a":
+			var crash = new Audio("sounds/crash.mp3");
+			crash.play();
+			break;
+
+		case "s":
+			var tom1 = new Audio("sounds/tom-1.mp3");
+			tom1.play();
+			break;
+
+		case "d":
+			var tom2 = new Audio("sounds/tom-2.mp3");
+			tom2.play();
+			break;
+
+		case "f":
+			var kick = new Audio("sounds/kick-bass.mp3");
+			kick.play();
+			break;
+
+		case "j":
+			var snare = new Audio("sounds/snare.mp3");
+			snare.play();
+			break;
+
+		case "k":
+			var tom3 = new Audio("sounds/tom-3.mp3");
+			tom3.play();
+			break;
+
+		case "l":
+			var tom4 = new Audio("sounds/tom-4.mp3");
+			tom4.play();
+			break;
+
+		default:
+			console.log(event.key);
+			break;
+	}
 }
+
+/*
+function handleClick() {
+	var buttonInnerHTML = this.innerHTML;
+
+	switch (buttonInnerHTML) {
+		case "a":
+			var crash = new Audio("sounds/crash.mp3");
+			crash.play();
+			break;
+
+		case "s":
+			var tom1 = new Audio("sounds/tom-1.mp3");
+			tom1.play();
+			break;
+
+		case "d":
+			var tom2 = new Audio("sounds/tom-2.mp3");
+			tom2.play();
+			break;
+
+		case "f":
+			var kick = new Audio("sounds/kick-bass.mp3");
+			kick.play();
+			break;
+
+		case "j":
+			var snare = new Audio("sounds/snare.mp3");
+			snare.play();
+			break;
+
+		case "k":
+			var tom3 = new Audio("sounds/tom-3.mp3");
+			tom3.play();
+			break;
+
+		case "l":
+			var tom4 = new Audio("sounds/tom-4.mp3");
+			tom4.play();
+			break;
+
+		default:
+			console.log(buttonInnerHTML);
+			break;
+  }} */
